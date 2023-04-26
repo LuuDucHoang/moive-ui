0b5bff2a25895097fa4f2de745d9889771cbe2a2
LuuDucHoang <livetolove2736@gmail.com>
2023-04-26 10:37:35 +0700

    sua url => env

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/',
+    // baseURL: 'https://api.themoviedb.org/3/',
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 export const get = async (path, options = {}) => {
     const response = await request.get(path, options);
